3fa9e9401acf73b41f95a5a2374b593336a9282a
Ayesha <ayeshaiqbal2592@gmail.com>
2023-05-13 16:43:59 +0500

    Changed Font Size

--- src/pages/Home/components/Hero.js
+++ src/pages/Home/components/Hero.js
@@ -5,7 +5,7 @@ export const Hero = () => {
   return (
     <section className="flex flex-col lg:flex-row items-center gap-x-20 mt-8">
         <div className="my-5 lg:mx-0 mx-5">
-            <h1 className="text-6xl font-medium dark:text-white mb-4">Learn from the Best</h1>
+            <h1 className="text-5xl md:text-6xl font-medium dark:text-white mb-4">Learn from the Best</h1>
             <p className="text-lg dark:text-white mb-6">Whether you're looking to build a new career, improve your current job prospects, or simply pursue a personal passion, we have the course for you. Plus, with our interactive platform and supportive community, you'll have all the resources you need to succeed. So why wait? Start your learning journey today and unlock your full potential.</p>
             <Link to="/products">
               <button className="border border-gray-200 dark:border-gray-700 rounded-lg px-4 py-2 text-lg lg:text-xl bg-logo text-white">Explore Courses</button>
